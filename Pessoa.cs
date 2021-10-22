12b31ae01d0adaf2f1e0c3185685d5bdfdcc9507
Jorgeteixeira00 <83607513+Jorgeteixeira00@users.noreply.github.com>
2021-10-21 22:59:54 -0300

    Add project files.

---
+++ Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pooCalculaIdade
+{
+    class Pessoa
+    {
+        private int _anoNascimento;
+        public int anoNascimento { get { return this._anoNascimento; } set { this._anoNascimento = value; } }
+
+        private string _nome;
+
+        public string nome { get { return this._nome; } set { this._nome = value.ToUpper(); } }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Nome:{this.nome}");
+            Console.WriteLine($"Ano de nascimento:{anoNascimento}");
+            Console.WriteLine($"Idade:{this.CalcularIdade()} anos");
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime date = DateTime.Now;
+            int ano = date.Year;
+            int idade = ano - this.anoNascimento;
+            return idade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pooCalculaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular idade de uma pessoa");
+            Pessoa pessoa = new();
+
+            Console.Write("Digite seu nome:");
+            pessoa.nome = Console.ReadLine();
+            Console.Write("Ano de nascimento:");
+            pessoa.anoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            pessoa.ExibirDados();
+        }
+    }
+}
